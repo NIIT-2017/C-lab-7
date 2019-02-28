9da1b7e6e791fbb90778f597d1c07590e06d8377
Anton Kapralov <akapralsheikan@gmail.com>
2019-02-28 18:32:54 +0400

    upd

--- src/main3.c
+++ src/main3.c
@@ -1,31 +1,31 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include "task3.h"
-//
-//int main(int argc, char* argv[])
-//{
-//	if (argc < 2)
-//	{
-//		printf("wrong format\n");
-//		return 1;
-//	}
-//	FILE *book = fopen(argv[1], "r");
-//	if (!book)
-//	{
-//		printf("cannot open %s\n", argv[1]);
-//		return 2;
-//	}
-//
-//	float frequenciesArray[MAX_SYM_COUNT] = { 0 };
-//	int SYMcount = MakeFreqsArray(book, frequenciesArray);
-//
-//	SYM* SYMarray = (SYM*)malloc(SYMcount * sizeof(SYM*));
-//
-//	FillingSYMarray(SYMarray, frequenciesArray);
-//
-//	PrintSYMarray(SYMarray);
-//
-//	fclose(book);
-//	free(SYMarray);
-//	return 0;
-//}
+#include <stdio.h>
+#include <stdlib.h>
+#include "task3.h"
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2)
+	{
+		printf("wrong format\n");
+		return 1;
+	}
+	FILE *book = fopen(argv[1], "r");
+	if (!book)
+	{
+		printf("cannot open %s\n", argv[1]);
+		return 2;
+	}
+
+	float frequenciesArray[MAX_SYM_COUNT] = { 0 };
+	int SYMcount = MakeFreqsArray(book, frequenciesArray);
+
+	SYM* SYMarray = (SYM*)malloc(SYMcount * sizeof(SYM*));
+
+	FillingSYMarray(SYMarray, frequenciesArray);
+
+	PrintSYMarray(SYMarray);
+
+	fclose(book);
+	free(SYMarray);
+	return 0;
+}
