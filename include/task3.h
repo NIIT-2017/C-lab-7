0da55bf4f049066560e7187db72c4d35072a6134
lerkas <44975578+lerkas@users.noreply.github.com>
2019-03-01 20:48:16 +0300

    Update task3.h

--- include/task3.h
+++ include/task3.h
@@ -1,14 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-struct SYM                    //представление символа
+struct SYM                           //представление символа
 {
-        unsigned char ch;     //ASCII-код
-        float freq;           //частота встречаетмости
-        char code[256];       //массив для нового кода
-        SYM *left;            //левый потомок в дереве
-        SYM *right;           //правый потомок в дереве
+        unsigned char ch;            //ASCII-код
+        float freq;                  //частота встречаетмости
+        char code[256];              //массив для нового кода
+        struct SYM *left;            //левый потомок в дереве
+        struct SYM *right;           //правый потомок в дереве
 };
 
-struct SYM *buildTree(struct SYM *psym[],int N);    //рeкурсивная функция создания дерева Хаффмана
+struct SYM *buildTree(struct SYM *psym[],int N);
+
