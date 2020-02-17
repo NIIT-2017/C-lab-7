d0805f18c9f522ab95964245022b864e92ca2556
Mikhail Treushnikov <www.mor.ry@mail.ru>
2020-02-17 10:02:01 +0300

    Lab 7 is done!

---
+++ include/task3.h
@@ -0,0 +1,8 @@
+﻿struct SYM // представление символа
+{
+	unsigned char ch; // ASCII-код
+	float freq; // частота встречаемости
+};
+
+int cmp(const void* a, const void* b);
+void print(struct SYM* syms);
---
+++ src/main1.cpp
@@ -0,0 +1,48 @@
+#include "task1.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+	FILE* fp;
+	int count = 0;
+	char buf[512];
+	PITEM head, tail, item;
+	fp = fopen("fips10_4.csv", "rt");
+	if (!fp) {
+		perror("File names.csv:");
+		exit(1);
+	}
+	fgets(buf, 512, fp);
+	while (fgets(buf, 512, fp))
+	{
+		if (count == 0)
+		{
+			head = createList(createName(buf));
+			tail = head;
+		}
+		else
+		{
+			tail = addToTail(tail, createName(buf));
+		}
+		count++;
+	}
+	fclose(fp);
+	printf("Total items: %d\n", countList(head));
+	while (head)
+	{
+		item = findByCountry(head, (char*)"TH");
+		if (item == NULL)
+			printf("End!\n");
+		else
+			printName(item);
+		head = item->next;
+	}
+	//item = findByRegion(head, (char*)"Harare");
+	//if (item == NULL)
+	//	printf("Not found!\n");
+	//else
+	//	printName(item);
+	return 0;
+}
